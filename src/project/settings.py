ad1e739c58467a6f1b37d7e0d289ab1fc6a5f2e0
Praneeth Kolanu <k.praneeth1199@iitkgp.ac.in>
2020-12-10 09:38:56 +0530

    Added screenshots of successfully running project and also added a gitignore file to the repo

--- src/project/settings.py
+++ src/project/settings.py
@@ -81,13 +81,9 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME': 'db_name',
-    'USER': 'name',
-    'PASSWORD': '',
-    'HOST': 'localhost',
-    'PORT': '',
-}
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 

422733dd52c8ba106582922d665ff39bb9229db1
Praneeth Kolanu <k.praneeth1199@iitkgp.ac.in>
2020-12-10 09:12:30 +0530

    Added a gitignore file and changed db settings in settings.py as per the readme.md

--- src/project/settings.py
+++ src/project/settings.py
@@ -81,9 +81,13 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'hotel_manage',
+    'USER': 'postgres',
+    'PASSWORD': 'praneeth19',
+    'HOST': 'localhost',
+    'PORT': '5432',
+}
 }
 
 
